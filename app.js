16d24d1c10773d7fac606992a5dd4e80874e1867
Hieu <hieu@render.com>
2023-05-18 11:08:55 -0700

    inc-1250 testing

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ const html = `
         });
       }, 500);
     </script>
+    <script>
+const xhr = new XMLHttpRequest();
+for (let i = 0; i < 100; i++) {
+  xhr.open("GET", "https://inc-1250-testing.onrender.com");
+  xhr.send();
+}
+    </script>
     <style>
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
       @font-face {
